bb14d55e31a357ec0f277bc3bd156aa45ff67903
Suporte - Alex <101653857+Lanzeira@users.noreply.github.com>
2023-03-31 18:02:09 -0300

    Atualização repositório JavaScript

---
+++ 12_op_comparacao/js/scripts.js
@@ -0,0 +1,32 @@
+var idade = 15;
+var posssuiCarro = 1; 
+
+if(idade >= 18) {
+    console.log("o usuario pode fazer a carteira");
+}
+if(idade <= 17) {
+    console.log("O usuário não pode ter a carteira");
+
+}
+if(posssuiCarro) {
+    console.log("O usuario já pode andar de carro");
+}
+
+var nome = "Matheus";
+
+if(nome == "Matheus") {
+    console.log("O seu nome é Matheus");
+}
+
+if(nome != "Matheus") {
+    console.log("o nome não é Matheus");
+}
+
+if(20 > 100) {
+    console.log("Passou");
+}
+
+if(100 < 20) {
+    console.log("Passou");
+}
+
