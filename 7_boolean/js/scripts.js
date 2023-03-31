6414b24384e4452a0dcbdd7479bff7f8c9c7d659
Suporte - Alex <101653857+Lanzeira@users.noreply.github.com>
2023-03-31 16:19:29 -0300

    Atualização do Treinamento JavaScript

---
+++ 7_boolean/js/scripts.js
@@ -0,0 +1,12 @@
+var verddeiro = true;
+
+console.log(verddeiro);
+console.log(typeof verddeiro);
+
+console.log(typeof true);
+console.log(typeof false);
+
+var falso = false;
+
+console.log(false);
+console.log(typeof falso);
---
+++ 8_null_underfined/js/scripts.js
@@ -0,0 +1,15 @@
+// hoisting - içamento
+console.log(sobrenome);
+console.log(numero);
+
+var nome = null;
+var sobrenome = 'Basttisti';
+
+console.log(nome);
+console.log(sobrenome);
+
+nome = "Matheus";
+
+console.log(nome);
+
+var numero = 5;
