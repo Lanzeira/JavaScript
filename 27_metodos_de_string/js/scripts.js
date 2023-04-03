953379fd9d18ee1aa8a372d1b1631a5d00bbed71
Suporte - Alex <101653857+Lanzeira@users.noreply.github.com>
2023-04-03 18:01:29 -0300

    Atualizei repositório até exemplo 29

---
+++ 27_metodos_de_string/js/scripts.js
@@ -0,0 +1,25 @@
+// length
+var nome = "Matheus";
+
+console.log(nome.length);
+
+var obj = "bola";
+console.log(obj.length);
+
+// indexOf
+console.log(nome[2]);
+
+var frase = "0 rato roeu a roupa do rei de Roma";
+
+console.log(frase.indexOf("roeu"));
+
+// slice
+var roeu = frase.slice(7, 11);
+
+console.log(roeu);
+
+// Replace
+
+var novaFrase = frase.replace("roe","teste");
+
+console.log(novaFrase);
---
+++ 29_metodos_de_array/js/scripts.js
@@ -0,0 +1,30 @@
+// length
+var arr = [1,2,3,4,5];
+
+console.log(arr.length);
+
+// push
+arr.push(6);
+arr.push('Qualquer coisa');
+
+console.log(arr);
+
+// pop 
+arr.pop();
+
+console.log(arr);
+
+// unshift
+arr.unshift(0);
+arr.unshift('teste');
+
+console.log(arr);
+
+// acessar o último elemento
+console.log(arr[arr.length - 1]);
+
+
+// isArray verificação de dado antes da manipulaçao
+console.log(Array.isArray(5));
+
+console.log(Array.isArray(arr));
